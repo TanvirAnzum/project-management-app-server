fe7ed64ff135f8fd4b3a806fb8a8e89aa5fcf83e
unknown <tanviranzum14@gmail.com>
2022-09-27 20:06:37 +0600

    added

---
+++ db.js
@@ -0,0 +1,11 @@
+const users = require("./data/users.json");
+const projects = require("./data/projects.json");
+const teams = require("./data/teams.json");
+
+
+module.exports = {
+  users,
+  projects,
+  teams
+}
+
